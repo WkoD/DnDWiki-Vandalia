8f0205a4a425e7158b2158a856a3d7a504a9c8ad
Christopher Pawek <christopher.pawek@itz-rostock.de>
2020-08-06 13:58:27 +0200

    added template class and style to subtiddler macro

--- tiddlers/$___my_Macro_SubTiddler.js
+++ tiddlers/$___my_Macro_SubTiddler.js
@@ -15,18 +15,30 @@ exports.params = [
    { name: "title"},
    { name: "sub" },
    { name: "heading" },
-   { name: "tags" }
+   { name: "tags" },
+   { name: "template" },
+   { name: "style" }
 ];
 
 /*
 Run the macro
 */
-exports.run = function(title, sub, heading, tags) {
+exports.run = function(title, sub, heading, tags, template, style) {
    var tiddler = this.wiki.getTiddler(title + "#" + sub);
    var parent = this.wiki.getTiddler(title);
    var ret = "";
    
    if (tiddler) {
+	   if (template) {
+		  ret += "<p class=\"" + template;
+		  
+		  if (style) {
+			 ret += "\" style=\"" + style;
+		  }
+		  
+		  ret += "\">";
+	   }
+	   
 	   if (heading) {
 		  ret += "<h3>" + heading + "</h3>";
 	   }
@@ -40,6 +52,10 @@ exports.run = function(title, sub, heading, tags) {
 	   }
 	   
        ret += "<$transclude tiddler=\"" + tiddler.fields.title + "\" mode=\"block\"/>";
+	   
+	   if (template) {
+	      ret += "</p>";
+	   }
    }
     
    return ret;
